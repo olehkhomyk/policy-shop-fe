cedf0e489c14e16db8b3e0576616f7326fd5587f
olehkhomyk <oleh.khomyk@gmail.com>
2019-01-20 15:30:27 +0200

    Added og tag

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { landingState } from './landing/landing.state';
 import { defaultState } from './other/default.state';
-import { orderState } from './order/order.state';
 import { changeRating } from './change-rating/change-rating.state';
 import { successState } from './success/success.state';
 import { doOrderState } from './do-order-mobile/do-order-mobile.state';
@@ -12,7 +11,6 @@ import { handleLiqpayState } from './handle-liqpay/handle-liqpay.state';
 const routes: Routes = [
   defaultState,
   landingState,
-  // orderState,
   changeRating,
   successState,
   doOrderState,
